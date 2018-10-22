d6a7a466a0ba252ff865d56076d5a46be24c2248
helenlavr <helenlav@stud.ntnu.no>
2018-10-22 13:34:01 +0200

    lagt inn footer og navbar i main, redigert main til riktig pos. q quit
    git commit
    hallooo

--- js/footer.js
+++ js/footer.js
@@ -1,3 +1,9 @@
-$(function(){
-    $("#footer").load("footer.html"); 
-  });
+window.addEventListener('load', function(){
+  function load_home() { 
+    document.getElementById("footer").innerHTML='<object type="text/html" data="footer.html" ></object>';
+  }
+  load_home();
+})
+
+
+
--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,12 @@
 
-function includeHTML(htmlname){
-    //her skal vi ydeligvis kunne bruke en greie fra w3schools
-}
+window.addEventListener('load', function(){
+    function load_home() { 
+      document.getElementById("navbar").innerHTML='<object type="text/html" data="navbar.html" ></object>';
+    }
+    load_home();
+  
+  })
+
 
 function myFunction(x) {
     x.classList.toggle("change");
@@ -10,8 +15,8 @@ function myFunction(x) {
 function toggleHide(element){
     element.classList.toggle("hidden");
 }
-let menu = document.getElementById('hamburgermenu');
-let menuContent= document.getElementById('hamburgercontent');
+let menu = document.getElementById('hamburgermenu1');
+let menuContent= document.getElementById('hamburgercontent1');
 
 menu.addEventListener('click', function() {
     toggleHide(menuContent)
