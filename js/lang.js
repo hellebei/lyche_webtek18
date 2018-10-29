0f9fd1375cb37884f38ffe1cd084822e85e92411
Helle Børset Eidissen <helleeidissen@gmail.com>
2018-10-29 11:31:54 +0100

    lang

--- js/lang.js
+++ js/lang.js
@@ -1,28 +1,20 @@
-function changeLang(){
-    /*console.log("In change lang")
-    let listNo = document.getElementsByClassName("no")
-    console.log("List of elem with no-tag")
-    console.log(listNo)
-    let listEn = document.getElementsByClassName("en")
-    console.log("List of elem with en-tag")
-    console.log(listEn)
-    for (element in listNo){
-        console.log("element")
-        console.log(element)
-        element.style.display = "none"
-    }
-    for (element in listEn){
-        console.log("element")
-        console.log(element)
-        element.style.display = ""
-    }
 
-    style.display = "none";
-    document.getElementsById("en").className.style.display = "block";*/
+if(currentLang = "en"){
+    changeLangtoEn()
+}
+if(currentLang = "no"){
+    changeLangtoNo()
+}
+
+
+function changeLangtoEn(){
     document.getElementById("no").style.display = "none"
     document.getElementById("en").style.display = "inherit"
+    let currentLang = "en"
+}
 
-
-
-
+function changeLangtoNo(){
+    document.getElementById("en").style.display = "none"
+    document.getElementById("no").style.display = "inherit"
+    let currentLang = "no"
 }
