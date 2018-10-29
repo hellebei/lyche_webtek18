932623395c9f62f6bc2365988392683e447e9686
helenlavr <helenlav@stud.ntnu.no>
2018-10-29 20:42:16 +0100

    trying to fix navbar. fuck this.

--- js/navbar.js
+++ js/navbar.js
@@ -1,13 +1,10 @@
-
-
-window.addEventListener('load', function(){
+/*window.addEventListener('load', function(){
     function load_home() { 
       document.getElementById("navbar_imported").innerHTML='<object id = "importNav" type="text/html" data="navbar.html" ></object>';
     }
     load_home();
   
   })
-
 
 function myFunction(x) {
     x.classList.toggle("change");
@@ -16,56 +13,59 @@ function myFunction(x) {
 function toggleHide(element){
     element.classList.toggle("hidden");
 }
+
 let menu = document.getElementById("hamburgermenu");
 let menuContent= document.getElementById("hamburgercontent");
 
 menu.addEventListener('click', function() {
     toggleHide(menuContent)
     myFunction(menu)
-})
-
-
-
-
+})*/
 
 document.getElementById("navbar").innerHTML = 
 `
-    
 <nav class="navmain" id="fixed-nav">
 
-<a href="#"><img id = "lyche-logo" src="img/logo.png" alt="Logo for Lyche"></a>
+    <a href="#"><img id = "lyche-logo" src="img/logo.png" alt="Logo for Lyche"></a>
 
-<div id="session-status">
-    <a class = "changelangNO" href="/no">NO</a>
-    <a class = "line" href=""> | </a>
-    <a class="changelangEN" href="/en">EN</a>
-</div>
+    <div id="session-status">
+        <a class = "changelangNO" href="/no">NO</a>
+        <a class = "line" href=""> | </a>
+        <a class="changelangEN" href="/en">EN</a>
+    </div>
 
-<a href="https://www.samfundet.no/"> <img id="samflogo"src="img/samf_logo.png" alt="Logo samf"> </a>
+    <a href="https://www.samfundet.no/"> <img id="samflogo"src="img/samf_logo.png" alt="Logo samf"> </a>
 
-</div>
 </nav>
-`
 
+`
 const injectedNavbar = document.getElementById("fixed-nav");
 console.log(injectedNavbar);
 const hamburgermenu = document.createElement("div");
 for (let i = 0; i < 3; i++){
     const bar = document.createElement("div");
     let classname = "bar"+ (i+1);
-    bar.setAttribute("class", classname);   
+    bar.setAttribute("class", classname);
 }
 
 const hamburgercontent = document.createElement("div");
-const 
+hamburgercontent.setAttribute("id", "hamburgercontent")
+hamburgercontent.setAttribute("class", "hidden");
 
+const ulContent = document.createElement("ul");
 
-<div id="hamburgercontent" class="hidden">
-    <ul>
-        <li><a href="#" class="item_menu">hjem</a></li>
-        <li><a href="#" class="item_menu">meny</a></li>
-        <li><a href="#" class="item_menu">om</a></li>
-        <li><a href="#" class="item_menu">kontakt</a></li>
-        <li><a href="#" class="item_menu">reservasjon</a></li>
-        <li><a href="#" class="item_menu">info</a></li>
-    </ul>
+const names = ["Home", "Menu", "About", "Contact", "Reservation", "Information"];
+
+for (let i=0; i < names.length; i++){
+    const li = document.createElement("li");
+    const a = document.createElement("a")
+    a.setAttribute("class", "item_menu");
+    let liName = document.createTextNode(names[i].toLowerCase());  
+    a.setAttribute("href", liName+".html")
+    a.appendChild(liName)
+    li.appendChild(a)
+    ulContent.appendChild(li)
+}
+
+hamburgercontent.appendChild(ulContent);
+ulContent.style.listStyleType = "none";
