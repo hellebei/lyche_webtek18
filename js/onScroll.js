434e050e555f51b5d53455eef3fc8cea0cb44fcf
Helle Børset Eidissen <helleeidissen@gmail.com>
2018-10-29 11:00:22 +0100

    funker litt

--- js/onScroll.js
+++ js/onScroll.js
@@ -1,26 +1,29 @@
-var scrollPos = 0;
-var dir = "none";
+let scrollPos = 0;
+let page = 1; 
+let dir = "none";
 
 function scrollWinDown(){
     window.scrollBy({
-        top: window.innerHeight,
+        top: document.body.getBoundingClientRect().top + window.innerHeight,
         left: 0, 
         behavior: 'smooth'
     }
         )
-    setTimeout(function(){console.log("Timeout")}, 4000)
+        page ++; 
+    setTimeout(function(){}, 4000)
     scrollPos = document.body.getBoundingClientRect().top;
     
 }
 
 function scrollWinUp(){
     window.scrollBy({
-        top: - window.innerHeight,
+        top: document.body.getBoundingClientRect().top - window.innerHeight,
         left: 0, 
         behavior: 'smooth'
     }
         )
-    setTimeout(function(){console.log("Timeout")}, 4000)
+        page --; 
+    setTimeout(function(){}, 4000)
     scrollPos = document.body.getBoundingClientRect().top;
     
 }
@@ -36,24 +39,19 @@ function scrollToPos(x){
 
 // adding scroll event
 window.addEventListener('scroll', function(){
+    //setTimeout(function(){console.log("Timeout")}, 20000000)
 // detects new state and compares it with the new one
-console.log((document.body.getBoundingClientRect()).top)
-console.log(scrollPos)
-if ((document.body.getBoundingClientRect()).top > scrollPos){
-        dir = 'UP';}
-    else if((document.body.getBoundingClientRect()).top < scrollPos) {
-        dir = 'DOWN';}
+console.log("b cient: " + (document.body.getBoundingClientRect()).top)
+console.log("scrollpos: " + scrollPos)
+if ((document.body.getBoundingClientRect()).top > scrollPos+20){
+    console.log("IN UP")
+    scrollWinUp(); 
+    }
+else if((document.body.getBoundingClientRect()).top < scrollPos-20) {
+    console.log("IN DOWN")
+    scrollWinDown(); 
+    }
     
-if (dir === "UP"){
-        console.log("IN UP")
-        scrollWinUp(); 
-        dir = "none"
-    }
-else if(dir === "DOWN"){
-        console.log("IN DOWN")
-        scrollWinDown(); 
-        dir = "none"
-    }
-    setTimeout(function(){console.log("Timeout")}, 4000)
+    
     // saves the new position for iteration.
 });
