422af38b411741a593cdf989d2b0fe7cf553fc4f
johanor <johanna.norum@hotmail.com>
2018-11-07 15:39:39 +0100

    trying to make contact responsive + small changes in isOpen.js

--- js/isOpen.js
+++ js/isOpen.js
@@ -6,13 +6,13 @@ time = d.getHours();
 
 
 function openingNo () {
-    if (time >= 16 && time <=23 && day <= 4) {
+    if (time >= 16 && time <23 && day <= 4) {
     document.getElementById("openNo").innerHTML = "Åpent! Stenger 23";
     }
-    else if ((time >= 16) && ((day===5) || (day===6)) || ((time <= 2) && ((day=6) || (day===7)))) {
+    else if ((time >= 16) && ((day===5) || (day===6)) || ((time < 2) && ((day=6) || (day===7)))) {
     document.getElementById("openNo").innerHTML = "Åpent! Stenger 02";
     }
-    else if ((time >= 16) && (time <= 22) && (day === 7)) {
+    else if ((time >= 16) && (time < 22) && (day === 7)) {
         document.getElementById("openNo").innerHTML = "Åpent! Stenger 22";
     }
     else {
