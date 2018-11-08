aba92a9e29618178eb34b22eef2d6d390cd4b2da
iahuus <43422172+iahuus@users.noreply.github.com>
2018-11-08 11:26:18 +0100

    responsive design

--- js/form.js
+++ js/form.js
@@ -3,47 +3,41 @@
 var tommorow = new Date();
 tommorow.setDate(tommorow.getDate() + 1);
 const time = tommorow.toISOString().split('T')[0];
-document.getElementsByName("date")[0].setAttribute('min', time);
-
+const dates = document.getElementsByName("date");
+for (let i = 0; i < dates.length; i++) {
+    dates[i].setAttribute('min', time);
+}
 
 
 //validates email adress on change. Not norwegian chars
 function ValidateEmail(){
-    let mailInput = document.getElementById("mail");
-    console.log(mailInput);
-    
-    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if(mailInput.value.match(mailformat))
-    {
-        mailInput.focus();
-        return true;
-    }
-    else
-    {
-        alert("You have entered an invalid email address!");
-        mailInput.focus();
-        return false;
-    }
-}
 
+    let mailInput = document.getElementsByName("email");
+    let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-function getInformation() {
-    var inputs = document.getElementById("form1").elements;
-// iterate over the form controls
-    for (i = 0; i < inputs.length; i++) {
-        console.log(inputs[i]);
-        /* (inputs[i].nodeName === "INPUT" && inputs[i].type === "text") {
-            inputs[i].value.toLocaleUpperCase();  // update text input
-        }*/
+    for( let j = 1; j < mailInput.length; j++){
+        if(mailInput[j].value.match(mailformat)){
+            mailInput[j].focus();
+            return true;
+        }
+        else
+        {
+            alert("You have entered an invalid email address!");
+            mailInput[j].focus();
+            return false;
+        }
+
     }
 }
 
-function confirm_order_no() {
-    var myWindow = window.open("confirmation.html");
+function confirm_order_no(){
+    if (ValidateEmail()) {
+        let myWindow = window.open("confirmation.html");
+    }
 }
 
 function confirm_order_en(){
-    var myWindow = window.open("confirmation.html");
-
+    if (ValidateEmail()) {
+        let myWindow = window.open("confirmation.html");
+    }
 }
-
